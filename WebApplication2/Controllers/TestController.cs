a2cdf6c2d1ec2da60b3ab9030cfa21362c55c4b8
janit <janit@BROKEN>
2025-03-26 09:52:45 +0000

    Add project files.

---
+++ WebApplication2/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RazorEngine;
+
+namespace WebApplication2.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Index(string userInput)
+        {
+            ViewBag.UserText = userInput;
+            ViewBag.DangerousOutput = Razor.Parse("<h3>" + userInput + "</h3>");
+            return View();
+        }
+    }
+}
